12933be2720f88aacf74a4f56f8abc91fcc5e3e0
imat19 <vergara.reymart82@gmail.com>
2023-02-17 18:55:06 +0800

    added about and projects

---
+++ src/app/layout.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import styles from "./page.module.css"
+
+export default function RootLayout({ children }) {
+ 
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossOrigin="anonymous"></link>
+        </head>  
+      <body>
+
+    
+        {children}
+  <script src="  https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossOrigin="anonymous"></script>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,145 @@
+import Image from "next/image"
+import raymart from "../../public/images/raymart.png"
+import blacklogo from "../../public/images/blacklogo.png"
+
+export default function Home() {
+  return (
+    <main>
+
+      <nav className="navbar navbar-expand-lg bg-white">
+        <div className="container-lg">
+          <a className="navbar-brand" href="#">
+            <Image className="img-fluid"
+              src={blacklogo}
+              alt="Picture of the author"
+              width={50}
+              height={50}
+              placeholder="blur"
+            />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav text-center  mx-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Education</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* Hero section */}
+      <div className="container-lg shadow">
+        <div className="row d-flex align-items-center bg-light pt-5">
+          <div className="col-md-6 col-12">
+            <div className="text-center">
+              <h1 className="fw-bold">Hi There 🖐🏻, <br />
+                I'm Raymart Vergara</h1>
+              <h3>Soon to be Web Developer</h3>
+              <button className="btn btn-dark px-3 py-2 mt-3 mx-2"><span className="h5 hfw-bold">About</span> </button>
+              <button className="btn bg-tranparent border border-dark px-3 py-2 mt-3 mx-2"><span className="h5 hfw-bold">Contact</span> </button>
+            </div>
+          </div>
+          <div className="col-md-6 col-12 text-center">
+            <Image className="img-fluid"
+              src={raymart}
+              alt="Picture of the author"
+              width={1000}
+              height={1000}
+              placeholder="blur"
+            />
+          </div>
+        </div>
+      </div>
+      {/* about me */}
+      <div className="container-lg pt-5">
+        <h1 className="border-start border-5 border-dark ps-4"> About Me</h1>
+        <div className="row">
+          <div className="col">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illum tempora voluptates magnam aut pariatur fugiat minus quas, molestiae est explicabo quos. Numquam voluptates optio pariatur similique hic aut sit?</p>
+          </div>
+        </div>
+      </div>
+      {/* Projects */}
+      <div className="container-lg pt-5">
+        <h1 className="border-start border-5 border-dark ps-4 mb-3"> Projects</h1>
+        <div className="row g-3">
+          <div className="col-6">
+            <div class="card">
+              <div class="row g-0">
+                <div class="col-md-6">
+                  <Image src={raymart} class="img-fluid rounded-start" alt="..." />
+                </div>
+                <div class="col-md-6">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div class="card ">
+              <div class="row g-0">
+                <div class="col-md-6">
+                  <Image src={raymart} class="img-fluid rounded-start" alt="..." />
+                </div>
+                <div class="col-md-6">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div class="card ">
+              <div class="row g-0">
+                <div class="col-md-6">
+                  <Image src={raymart} class="img-fluid rounded-start" alt="..." />
+                </div>
+                <div class="col-md-6">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div class="card ">
+              <div class="row g-0">
+                <div class="col-md-6">
+                  <Image src={raymart} class="img-fluid rounded-start" alt="..." />
+                </div>
+                <div class="col-md-6">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
