7206783e2f61db43bc90bcca307b778717b8eb0e
imat19 <vergara.reymart82@gmail.com>
2023-03-15 14:20:55 +0800

    aaa

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,47 +2,47 @@ import Image from "next/image"
 import raymart from "../../public/images/raymart.png"
 import whitelogo from "../../public/images/whitelogo.png"
 import wave from "../../public/images/wave.gif"
-import Link from "next/link"
+import styles from "./page.module.css"
 
 
 export default function Home() {
   return (
 
-    <main>
-        <nav class="navbar bg-dark" data-bs-theme="dark" id="home">
-          <Image className="img-fluid"
-            src={whitelogo}
-            alt="Picture of the author"
-            width={50}
-            height={50}
-            placeholder="blur"
-            id="home"
-          />
-          <div className="col text-center">
-          <ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#home">HOME</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#aboutme">ABOUT</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#projects">PROJECTS</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#contact">CONTACT</a>
-  </li>
-</ul>
-          </div>
-        </nav>
-    
+    <main className="bg-light">
+      <nav className="navbar bg-dark" data-bs-theme="dark" id="home">
+        <Image className="img-fluid"
+          src={whitelogo}
+          alt="Picture of the author"
+          width={50}
+          height={50}
+          placeholder="blur"
+          id="home"
+        />
+        <div className="col text-center">
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <a className="nav-link text-white active" aria-current="page" href="#home">HOME</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#aboutme">ABOUT</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#projects">PROJECTS</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#contact">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
 
       {/* Hero section */}
       <div className="container-lg shadow">
         <div className="row d-flex align-items-center bg-light pt-5" id="home">
           <div className="col-md-6 col-12 ">
             <div className="text-center">
-              <h1 className="fw-bold">Hi There,
+              <h1 className={styles.bold}>Hi There,
                 <Image className="img-fluid mb-0 ms-2 align-top "
                   src={wave}
                   alt="waving hand"
@@ -141,64 +141,33 @@ export default function Home() {
         <h1 className="border-start border-5 border-dark ps-4 mb-3" id="projects">Projects</h1>
         <div className="row g-3">
           <div className="col-6 col-12 col-md-6">
-            <div class="card shadow">
-              <div class="row g-0">
-                <div class="col-md-6">
-                  <Image src={raymart} class="img-fluid rounded-start" alt="..." />
+            <div className="card shadow">
+              <div className="row g-0">
+                <div className="col-md-7">
+                  <Image src="/images/Dreamfields.png" className="img-fluid rounded-start" alt="..." width={1500} height={1500} />
                 </div>
-                <div class="col-md-6">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-md-6">
-            <div class="card shadow">
-              <div class="row g-0">
-                <div class="col-md-6">
-                  <Image src={raymart} class="img-fluid rounded-start" alt="..." />
-                </div>
-                <div class="col-md-6">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-md-6">
-            <div class="card shadow">
-              <div class="row g-0">
-                <div class="col-md-6">
-                  <Image src={raymart} class="img-fluid rounded-start" alt="..." />
-                </div>
-                <div class="col-md-6">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <div className="col-md-5">
+                  <div className="card-body">
+                    <h5 className="card-title">Dream Fields</h5>
+                    <p className="card-text">This is a wider card with supporting text below as</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
           <div className="col-6 col-12 col-md-6">
-            <div class="card shadow">
-              <div class="row g-0">
-                <div class="col-md-6">
-                  <Image src={raymart} class="img-fluid rounded-start" alt="..." />
+            <div className="card shadow" height={1200} width={1200}>
+              <div className="row g-0">
+                <div className="col-md-7">
+                  <Image src="/images/Dreamfields.png" className="img-fluid rounded-start" alt="..." width={1500} height={1500} />
                 </div>
-                <div class="col-md-6">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <div className="col-md-5">
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                   </div>
                 </div>
               </div>
@@ -206,29 +175,38 @@ export default function Home() {
           </div>
         </div>
         <div className="container-lg pt-5 ps-0">
-          <h1 className="border-start border-5 border-dark ps-4" id="contact"> Contact Me</h1>
           <div className="row">
-            <div className="col">
+            <div className="col-md-6">
+              <h1 className="border-start border-5 border-dark ps-4" id="contact"> Contact Me</h1>
+              <div className="row g-3 mt-2 ms-4 mb-5">
+                <i className="fa-solid fa-phone"> <span className="text-dark text-decoration-none ms-3">+63 919 599 5618</span></i>
+                <i className="fa-brands fa-facebook"> <a className="text-dark text-decoration-none ms-3" href="https://www.facebook.com/rhey19/"><span> https://www.facebook.com/rhey19/</span> </a></i>
+                <i className="fa-solid fa-envelope"><a className="text-dark text-decoration-none ms-3" href="https://accounts.google.com/"><span> raymartvergara2023@gmail.com </span> </a></i>
+                <i className="fa-brands fa-linkedin"><a className="text-dark text-decoration-none ms-3" href="https://www.linkedin.com/in/raymart-vergara-b01241221/"><span> Raymart Vergara </span></a></i> 
+              </div>
+
+            </div>
+            <div className="col-md-6 mb-3">
+              <h1 className="border-start border-5 border-dark ps-4" id="message"> Leave a Message</h1>
               <form id="" action="https://formspree.io/f/xdovzbrg" method="POST">
-                <label for="email" className="form-label">Email address <span className="text-danger">*</span></label>
+                <label htmlFor="email" className="form-label">Email address <span className="text-danger">*</span></label>
                 <input className="form-control" name="Email" id="email" type="email" required />
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                <label for="email" className="form-label">Fullname  <span className="text-danger">*</span></label>
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 <div className="row">
-                  <div className="col-md-6 mb-3">
-                    <input className="form-control" name="firstName" id="firstName" type="text" placeholder="Firstname" required />
-                  </div>
-                  <div className="col-md-6 mb-3">
-                    <input className="form-control" name="lastName" id="lastName" type="text" placeholder="Lastname" required />
+                  <div className="my-3">
+                    <label htmlFor="email" className="form-label">Fullname <span className="text-danger">*</span></label>
+                    <input className="form-control" name="fullname" id="firstName" type="text" placeholder="Ex. Juan Dela Cruz" required />
                   </div>
                 </div>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                <button className="btn btn-dark" type="submit" pattern="[A-Za-z]">Submit</button>
+                <label htmlFor="message" className="form-label">Message</label>
+                <textarea className="form-control" id="messagebox" name="messagebox" type="text" rows="3"></textarea>
+                <button className="btn btn-dark my-3" type="submit">Submit</button>
               </form>
             </div>
           </div>
         </div>
       </div>
+      <footer className="text-center bg-dark text-white">Raymart Vergara</footer>
     </main>
   )
 
